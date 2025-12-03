297b5f15de1276296899c2b6d5fcff1746abbaa7
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2025-12-03 14:43:20 +0530

    Update models.js

--- src/models.js
+++ src/models.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+
 const StorageSchema = new mongoose.Schema({
   key: { type: String, required: true, unique: true, index: true },
   payload: { type: String, required: true },
@@ -7,7 +8,10 @@ const StorageSchema = new mongoose.Schema({
 
 const StatsSchema = new mongoose.Schema({
   _id: { type: String, default: 'global_stats' },
-  totalRequests: { type: Number, default: 0 },
+  allTime: {
+    get: { type: Number, default: 0 },
+    post: { type: Number, default: 0 }
+  },
   today: {
     date: { type: String, default: '' },
     get: { type: Number, default: 0 },
